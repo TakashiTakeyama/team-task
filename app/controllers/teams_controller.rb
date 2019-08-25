266885cc10f06485901e5829463023d7f37ba8c6
Takashi <Takashi@gmail.com>
2019-08-25 14:38:28 +0900

    不要な記述を削除

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -55,7 +55,7 @@ class TeamsController < ApplicationController
   def owner_change
     @team = Team.find_by(name: params[:team_id])
     @user = User.find_by(id: params[:id])
-    binding.pry
+    # binding.pry
     @team.update(owner_id: params[:id])
     redirect_to team_url(params[:team_id]), notice: '権限を移動しました！'
     # OwnerMailer.owner_mail(@user.email, @team).deliver
